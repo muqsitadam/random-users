83f4aecf449b86ba060d9a21bc629266f1cb08e8
muqsitadam <muqsitadam@gmail.com>
2022-11-21 10:52:48 +0100

    Updated project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
+import { FaHome, FaPhoneAlt, FaPortrait, FaEnvelope } from "react-icons/fa";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      pictureSrc: '',
+      name: '',
+      age: '',
+      email: '',
+      phoneNumber: '',
+      location: '',
+      loading: false
+    }
+  }
+
+ componentDidMount = () => {
+    fetch("https://randomuser.me/api/")
+    .then(res => res.json())
+    .then(data => {
+      const user = data.results[0]
+      console.log(user)
+      const {title, first, last} =  user.name
+      const {street,city,state,country} = user.location
+      this.setState({
+        pictureSrc: user.picture.large,
+        name: `${title} ${first} ${last}`,
+        age: user.dob.age,
+        email: user.email,
+        phoneNumber: user.phone,
+        location: `${street.number}, ${street.name}, ${city}, ${state}, ${country}`,
+        loading: true
+      })
+    })
+  }
+
+
+
+  render(){
+    const {pictureSrc,name,age,email,phoneNumber,location,loading} = this.state
+
+    if(!loading){
+      return(
+        <div id='loader'></div>
+      )
+    }else{
+      return (
+        <div className="text-center">
+          <div className='w-1/2'>
+            <img src={pictureSrc} alt={name}></img>
+            <p className='flex flex-row'><FaPortrait/>: {name}, {age}yrs</p>
+            <p><FaEnvelope/>: {email}</p>
+            <p><FaPhoneAlt/>: {phoneNumber}</p>
+            <p><FaHome/>: {location}</p>
+          </div>
+          <button className='button' onClick={() => window.location.reload(true)}>Random User</button>
+          {/* youssoufa moukoko */}
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
